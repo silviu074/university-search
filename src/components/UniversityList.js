85c618dcc4068adac30dc8f7c2d4b8c0f92fb19c
Silvio <silviu074@gmail.com>
2022-09-25 19:25:08 +0300

    correct data flow in console.log, working on render

--- src/components/UniversityList.js
+++ src/components/UniversityList.js
@@ -7,6 +7,7 @@ const UniversityList = () => {
     const [error, setError] = useState(false)
     const [university, setUniversity] = useState()
     const [searchWord, setSearchWord] = useState('')
+    const [results, setResults] = useState([])
 
     useEffect( () => {
       axios.get('https://raw.githubusercontent.com/Hipo/university-domains-list/master/world_universities_and_domains.json')
@@ -23,34 +24,39 @@ const UniversityList = () => {
         })
     }, [])
     
-
-    function searchFunction(x) {
+     function searchFunction(university, searchWord) {
       let results = []
       for(let i=0;i<=university.length;i++){
            if(typeof(university[i]) === 'undefined') break;
-           if (university[i].country.toLowerCase().includes(x.toLowerCase())){
-            console.log(university[i].country)
-            results = university[i]
-           }
+           if (university[i].name.toLowerCase().includes(searchWord.toLowerCase()))
+           {
+            // console.log(university[i].name)
+            results[i] = university[i]
+           } else results[i]=''
       }
+      results= results.filter(Boolean)
+      console.log(results)
       return results
     }
-   
-  
+    
 
   return (
     <div>
       
       {loading ? 'Loading' : null}
-        {error ? 'Something went wrong' : null}
+      {error ? 'Something went wrong' : null}
       <h1>Search</h1>
-      <input type='text' placeholder='search' 
-      onChange={e => {
-        setSearchWord(e.target.value)
-        }} />
-        {/* <button onClick={searchFunction(searchWord)} > Search</button> */}
-      
-        
+      <input 
+         type='text' 
+         placeholder='Type here' 
+         onChange={e => {
+           setSearchWord(e.target.value)
+           }} />
+      <button onClick={ () => setResults(searchFunction(university, searchWord))} > Search</button>
+       <div>
+         {results.map( ({results}, index) =>
+         <div key={index}><li>{results} {index}</li></div>)} 
+       </div>
     </div>
   )
 }
